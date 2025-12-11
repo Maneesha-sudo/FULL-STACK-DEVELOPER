5f737952ccb98b02c02d4fa732ee2e83c3295313
maneesha-sudo <matetimaneesha4@gmail.com>
2025-12-11 17:26:05 +0530

    assignment done

--- Module 3/Day_3_Standard-Project-Structure/Components/footer.js
+++
@@ -1,7 +0,0 @@
-export function footer(){
-    return`
-    <footer style="margin-top:20px; padding:10px; background:#eee; text-align:center;">
-            <p>© 2025 My App | All Rights Reserved</p>
-        </footer>
-    `;
-}
--- Module 3/Day_3_Standard-Project-Structure/Components/navbar.js
+++
@@ -1,9 +0,0 @@
-export function navbar() {
-    return`
-    <nav style="padding: 20px;">
-        <a href="./index.html">Home</a>
-        <a href="signup.html">Sign Up</a>
-        <a href="login.html">Login</a>
-        <a href="todos.html">Todo</a>
-    </nav> `
-    }
