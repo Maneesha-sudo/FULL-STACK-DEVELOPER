49d935ca131e85479f79930c288ec1c24e8fa2a8
maneesha-sudo <matetimaneesha4@gmail.com>
2025-12-11 18:29:53 +0530

    task done

---
+++ Module 3/q2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+import MainPage from "./MainPage";
+
+function App(){
+    return(
+        <div>
+            < MainPage/>
+        </div>
+    );
+}
+export default App;
---
+++ Module 3/q2/src/Content.jsx
@@ -0,0 +1,17 @@
+function Content() {
+  return (
+    <div
+      style={{
+        backgroundColor: "grey",
+        padding: "15px",
+        textAlign: "center",
+        color: "blue",
+        marginBottom: "20px" 
+      }}
+    >
+      <h2>This is Main Content</h2>
+    </div>
+  );
+}
+
+export default Content;
